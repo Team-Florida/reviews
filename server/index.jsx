e9c499f67777c3c1b0e7a58659c445812be501e6
Philip Hamner <philiphamner@gmail.com>
2020-01-27 12:02:18 -0800

    Add seeding script, fixed errors I had tried to move some functionality over to seedingScript.jsx file, but ran into some errors that I could not resolve.  For now, all of my database seeding functionality will exist within database/index.jsx.

--- server/index.jsx
+++ server/index.jsx
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const PORT = 3000;
+const PORT = 3001;
 const app = express();
 app.use(express.static(__dirname + '/../client/dist'));
 
